376b83a009bf964e4e341e62f80b2f16bc042cfa
Sabo <57690741+carloskbonet@users.noreply.github.com>
2021-11-24 14:52:19 -0300

    finalização dos modais de confimação

--- View/Adm/Create/register_adm.php
+++ View/Adm/Create/register_adm.php
@@ -15,44 +15,65 @@ include './View/Adm/navbar.php';
 
             <div class="card" style="width: 400px;">
                 <div class="overflow-auto card-body">
-                <form action="/signup" method="post">
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-at"></i></span>
+                    <form action="/signup" method="post">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-at"></i></span>
+                            </div>
+                            <input class="form-control" type="email" name="email-signup" placeholder="E-mail" minlength="8" maxlength="64" required>
                         </div>
-                        <input class="form-control" type="email" name="email-signup" placeholder="E-mail" minlength="8" maxlength="64" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input class="form-control" type="password" name="password-signup" placeholder="Senha" minlength="6" maxlength="32" required>
                         </div>
-                        <input class="form-control" type="password" name="password-signup" placeholder="Senha" minlength="6" maxlength="32" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-pen"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-pen"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="name-signup" placeholder="Nome" minlength="3" maxlength="128" required>
                         </div>
-                        <input class="form-control" type="text" name="name-signup" placeholder="Nome" minlength="3" maxlength="128" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="phone-signup" id="phone" placeholder="(99) 99999-9999" title="e.g (99) 99999-9999" pattern="^\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}$" required>
                         </div>
-                        <input class="form-control" type="text" name="phone-signup" id="phone" placeholder="(99) 99999-9999" title="e.g (99) 99999-9999" pattern="^\([0-9]{2}\)\s[0-9]{5}-[0-9]{4}$" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <select class="form-select w-100 h-100" name="status">
-                            <option value="false">User</option>
-                            <option value="true">Admin</option>
-                        </select>
-                    </div>
+                        <div class="input-group form-group">
+                            <select class="form-select w-100 h-100" name="status">
+                                <option value="false">User</option>
+                                <option value="true">Admin</option>
+                            </select>
+                        </div>
 
-                    <button type="submit" class="btn float-right btn-outline-secondary">Criar novo Usuário</button>
-                </form>
+                        <button type="button" class="btn float-right btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#myModal">Criar novo Usuário</button>
+
+                        <!-- The Modal -->
+                        <div class="modal fade" id="myModal">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Confirmar Cadastro</h4>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+                                        <button type="submit" class="btn btn-success" data-bs-dismiss="modal">Confirmar</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- View/Adm/Create/register_book.php
+++ View/Adm/Create/register_book.php
@@ -15,44 +15,65 @@ include './View/Adm/navbar.php';
 
             <div class="card" style="width: 400px;">
                 <div class="overflow-auto card-body">
-                <form action="/create-book" method="post">
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-book"></i></span>
+                    <form action="/create-book" method="post">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-book"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="name" placeholder="Nome" required>
                         </div>
-                        <input class="form-control" type="text" name="name" placeholder="Nome" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-align-left"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-align-left"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="description" placeholder="Descrição" required>
                         </div>
-                        <input class="form-control" type="text" name="description" placeholder="Descrição" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-ad"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-ad"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="author" placeholder="Autor" required>
                         </div>
-                        <input class="form-control" type="text" name="author" placeholder="Autor" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fab fa-fort-awesome"></i></span>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fab fa-fort-awesome"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="category" placeholder="Categoria" required>
                         </div>
-                        <input class="form-control" type="text" name="category" placeholder="Categoria" required>
-                    </div>
 
-                    <div class="input-group form-group">
-                        <select class="form-select w-100 h-100" name="status">
-                            <option>available</option>
-                            <option>unavailable</option>
-                        </select>
-                    </div>
+                        <div class="input-group form-group">
+                            <select class="form-select w-100 h-100" name="status">
+                                <option>available</option>
+                                <option>unavailable</option>
+                            </select>
+                        </div>
 
-                    <button type="submit" class="btn float-right btn-outline-secondary">Criar novo livro</button>
-                </form>
+                        <button type="button" class="btn float-right btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#myModal">Criar novo livro</button>
+
+                        <!-- The Modal -->
+                        <div class="modal fade" id="myModal">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Confirmar Cadastro</h4>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+                                        <button type="submit" class="btn btn-success" data-bs-dismiss="modal">Confirmar</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
